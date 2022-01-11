b231f0ef01d46bca55cde99c766435f3f844d60d
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-01-11 15:14:34 +0100

    one botto and 3 buttons useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import {useState} from 'react';
 
 const App = () => {
   
+  let [isHomePage, setToLanding] = useState(true);
   let [currentPage, switchPage] = useState("Home Page");
 
   return (
     <div>
-      <p>Hello!</p>
+      <h1>One button useState switch:</h1>
+      <button onClick={() => setToLanding(!isHomePage)}>Click to switch page!</button>
+      {isHomePage ? <h3>Home page</h3> : <h3>Landing page</h3>}
+      <hr/>
+
+      <h1>3 button useState switch:</h1>
       <button onClick={() => switchPage("Home Page")}>Home Page</button>
       <button onClick={() => switchPage("Landing Page")}>Landing Page</button>
       <button onClick={() => switchPage("About Page")}>About Page</button>
-      <h1>{currentPage}</h1>
+      <h3>{currentPage}</h3>
     </div>
   )
 }
