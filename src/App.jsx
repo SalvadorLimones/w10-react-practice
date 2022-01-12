217c391dc126d3788a056ab436a8269d56d37540
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-01-12 15:21:47 +0100

    add wednesday fun

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ const App = () => {
   
   let [isHomePage, setToLanding] = useState(true);
   let [currentPage, switchPage] = useState("Home Page");
+  const [currentValue, setValue] = useState("");
+
+  const cars = [
+    {brand: "Toyota", model: "Yaris", topSpeed: "190km/h"},
+    {brand: "Mercedes", model: "C63", topSpeed: "260km/h"},
+    {brand: "Mazda", model: "6", topSpeed: "210km/h"},
+    {brand: "Mazda", model: "Civic", topSpeed: "200km/h"},
+    {brand: "Audi", model: "A4", topSpeed: "220km/h"},
+  ]
+
+  console.log(currentValue);
 
   return (
     <div>
@@ -20,6 +31,17 @@ const App = () => {
       {currentPage === "Home Page" && <h3>Home Page</h3>}
       {currentPage === "Landing Page" && <h3>Landing Page</h3>}
       {currentPage === "About Page" && <h3>About Page</h3>}
+
+      {currentPage === "About Page" ? <h3>About Page</h3> : currentPage === "Landing Page" ? <h3>Landing Page</h3> : <h3>Home Page</h3>}
+
+      <hr/>
+
+        <input type="text" value={currentValue} onChange={e => setValue(e.target.value)}/>
+        <button disabled={currentValue.length<3} onClick={() => console.log(currentValue)}>Submit</button>
+        {currentValue.length < 3 && <p>Min 3 char!</p>}
+      
+      <hr/>
+        {cars.map( car => car.brand.startsWith(currentValue) && <div key={car.model}>{car.brand}-{car.model} ({car.topSpeed})</div>)}
     </div>
   )
 }
